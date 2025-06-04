d2636d087f884964170a71e122f39295e2dc8d40
yatashin <s-yata@creationline.com>
2025-06-04 12:12:31 +0900

    setup truth library

---
+++ src/test/java/com/samurai/TenDaysOrderCalculatorTest.java
@@ -0,0 +1,13 @@
+package com.samurai;
+
+import org.junit.jupiter.api.Test;
+
+import static com.google.common.truth.Truth.assertThat;
+
+class TenDaysOrderCalculatorTest {
+
+    @Test
+    void dailySplitBy() {
+        assertThat(1).isEqualTo(2);
+    }
+}
