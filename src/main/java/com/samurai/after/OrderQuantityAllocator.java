100bbf0b6dd596340c35306d7dbd8344a92cc3f2
s-yata <s-yata@creationline.com>
2025-06-07 07:34:27 +0900

    implemented allocator

---
+++ src/main/java/com/samurai/after/OrderQuantityAllocator.java
@@ -0,0 +1,25 @@
+package com.samurai.after;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class OrderQuantityAllocator {
+
+    public List<Map<OrderDate, OrderQuantity>> allocate(List<OrderDate> workingDays, OrderQuantity orderQuantity) {
+        var result = new ArrayList<Map<OrderDate, OrderQuantity>>();
+
+        int quotient = orderQuantity.quantity() / workingDays.size();
+        int remainder = orderQuantity.quantity() % workingDays.size();
+
+        for (var orderDate: workingDays) {
+            var extra = remainder > 0 ? 1 : 0;
+            result.add(Map.of(orderDate, new OrderQuantity(quotient + extra)));
+
+            remainder -= 1;
+        }
+
+        return result;
+    }
+}
